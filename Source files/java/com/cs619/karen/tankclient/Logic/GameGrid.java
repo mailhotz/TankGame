a75ab05992e9222867681db9438b171df9e02853
Zachary Mailhot <Mailhotz@yahoo.com>
2015-10-31 20:47:23 -0400

    Finished Tank Movement
    Functional tank movement

--- Source files/java/com/cs619/karen/tankclient/Logic/GameGrid.java
+++ Source files/java/com/cs619/karen/tankclient/Logic/GameGrid.java
@@ -1,5 +1,7 @@
 package com.cs619.karen.tankclient.Logic;
 
+import android.util.Log;
+
 import com.cs619.karen.tankclient.rest.BulletZoneRestClient;
 import com.cs619.karen.tankclient.ui.GridAdapter;
 import com.cs619.karen.tankclient.util.BooleanWrapper;
@@ -11,6 +13,8 @@ import com.cs619.karen.tankclient.util.LongWrapper;
  * Created by Zachary on 10/30/2015.
  */
 public class GameGrid {
+    private static final String TAG = "GameGrid";
+
     //Grid that holds values from server
     private int grid [][] = new int[16][16];
 
@@ -58,15 +62,17 @@ public class GameGrid {
         for(int i = 0; i < 15; i++) {
             for (int j = 0; j < 15; j++) {
                 long val = grid[i][j];
-                String valS = "" + val;
-                valS.valueOf(val);
-                System.out.println("Val as a string is: " + valS);
                 if(val >= 1000 && val <= 2000)
                     System.out.print(" W ");
                 else if(val >= 2000000 && val <= 3000000)
                     System.out.print(" B ");
-                else if(val >= 10000000 && val <= 20000000)
-                    System.out.print(" T ");
+                else if(val >= 10000000 && val <= 20000000) {
+                    String v = val + "";
+                    if(myID == Long.parseLong(v.substring(1,4)))
+                        System.out.print(" T ");
+                    else
+                        System.out.print(" t ");
+                }
                 else
                     System.out.print(" - ");
             }
@@ -85,4 +91,95 @@ public class GameGrid {
         return myID;
     }
 
+    //findTank looks for the tank of the given ID and returns a Tank if it has the same ID
+    //else it returns null
+    private Tank findTank(long id){
+        for(int i=0; i<16; i++)
+            for(int j=0; j<16; j++)
+                if( tGrid[i][j] instanceof Tank ) {
+                    Tank t = (Tank)tGrid[i][j];
+                    if ( id == t.getId() )
+                        return (Tank) tGrid[i][j];
+                }
+        return null;
+    }
+
+    //canMove takes a long and an int. The long the tankId that the player wants
+    //to move and the int is the direction that the player wants to move the tank
+    //Return: 1 - if the tank can make that move
+    //        0 - if the tank can make that rotation
+    //       -1 - if the tank can't do anything
+    public int canMove( long tId, int direction ){
+        Tank t = findTank(tId);
+        System.out.println( "You are facing " + t.getDirection() + " you want to go " + direction);
+        if( eqOrOp( t.getDirection(), direction ) ) {
+            System.out.println( "Moving" );
+            switch (direction) {
+                case 0: // move up case
+                    if (tGrid[t.getCol() - 1][t.getRow()].getVal() != 0)
+                        return -1;
+                    break;
+                case 2: // move right case
+                    if (tGrid[t.getCol()][t.getRow()+1].getVal() != 0)
+                        return -1;
+                    break;
+                case 4: // move down case
+                    if (tGrid[t.getCol()+1][t.getRow()].getVal() != 0)
+                        return -1;
+                    break;
+                case 6: // more left case
+                    if (tGrid[t.getCol()][t.getRow()-1].getVal() != 0)
+                        return -1;
+                    break;
+            }
+            return 1;
+        }
+        else{
+            System.out.println("Turning");
+            switch(direction){
+                case 0: // rotate to face up
+                    if( t.getDirection() == 4 )
+                        return -1;
+                    break;
+                case 2: // rotate to face right
+                    if( t.getDirection() == 6 )
+                        return -1;
+                    break;
+                case 4: // rotate to face down
+                    if( t.getDirection() == 0 )
+                        return -1;
+                    break;
+                case 6: // rotate to face left
+                    if( t.getDirection() == 2 )
+                        return -1;
+                    break;
+            }
+            return 0;
+        }
+    }
+
+    //canRotate takes a long and an int
+    //returns a boolean:
+    //                  true if the move was made
+    //                  false if the move was not made
+    public boolean canRotate( long tId, int direction ){
+        return true;
+    }
+
+    //eqOrOp( directions are equal or opposite )
+    //checks to see if the two directions that are fed into it are equal
+    //or opposite
+    //Returns a boolean
+    private boolean eqOrOp( int f, int s){
+        if( f == 0 && s == 4 || f == 4 && s == 0 ){
+            return true;
+        }
+        else if( f == 2 && s == 6 || f == 6 && s == 2 ){
+            return true;
+        }
+        else if( f == s )
+            return true;
+        else
+            return false;
+    }
 }
